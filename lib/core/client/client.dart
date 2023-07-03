19e77e13a4743bdc5cc051af6298c97d07e6103a
tun43p <git@tun43p.com>
2023-07-03 19:58:14 +0200

    feat: add redirect url to config file

--- lib/core/client/client.dart
+++ lib/core/client/client.dart
@@ -1,4 +1,5 @@
 class Client {
   static String id = const String.fromEnvironment('CLIENT_ID');
   static String secret = const String.fromEnvironment('CLIENT_SECRET');
+  static String redirect = const String.fromEnvironment('REDIRECT_URI');
 }
