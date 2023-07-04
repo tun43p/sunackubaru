5c729818daf29750c890d87d8f710c1effa66aca
tun43p <git@tun43p.com>
2023-07-04 19:02:05 +0200

    feat: update environment config

--- lib/core/client/client.dart
+++
@@ -1,4 +0,0 @@
-class Client {
-  static String id = const String.fromEnvironment('CLIENT_ID');
-  static String secret = const String.fromEnvironment('CLIENT_SECRET');
-}
---
+++ lib/core/config/config_constants.dart
@@ -0,0 +1,4 @@
+// ignore_for_file: unnecessary_const, we need const for environment variables
+
+const String clientId = const String.fromEnvironment('CLIENT_ID');
+const String clientSecret = const String.fromEnvironment('CLIENT_SECRET');
