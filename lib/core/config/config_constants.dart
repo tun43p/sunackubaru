e57fc13ab0231c109de9ebc90724e888e2e9183a
tun43p <git@tun43p.com>
2023-07-05 18:17:18 +0200

    chore: rename config to env

--- lib/core/config/config_constants.dart
+++
@@ -1,4 +0,0 @@
-// ignore_for_file: unnecessary_const, we need const for environment variables
-
-const String clientId = const String.fromEnvironment('CLIENT_ID');
-const String clientSecret = const String.fromEnvironment('CLIENT_SECRET');
